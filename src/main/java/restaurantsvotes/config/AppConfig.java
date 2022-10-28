66b41dbc017d7e186db76abf5e49b87bfd9c65d7
Nikita Dolbilov <nikita1100@bk.ru>
2022-10-28 16:41:16 +0300

    132

--- src/main/java/restaurantsvotes/config/AppConfig.java
+++ src/main/java/restaurantsvotes/config/AppConfig.java
@@ -1,17 +1,24 @@
-//package restaurantsvotes.config;
-//
+package restaurantsvotes.config;
+
 //import org.h2.tools.Server;
-//import org.springframework.cache.annotation.EnableCaching;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.sql.SQLException;
-//
-//@Configuration
-//@EnableCaching
-//public class AppConfig {
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.sql.SQLException;
+
+@Configuration
+@EnableCaching
+public class AppConfig {
 //    @Bean(initMethod = "start", destroyMethod = "stop")
 //    public Server h2Server() throws SQLException {
 //        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
 //    }
-//}
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+}
